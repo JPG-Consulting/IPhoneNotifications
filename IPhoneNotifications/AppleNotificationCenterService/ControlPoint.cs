694aaca6ee59642e535293eaae06bcdcc7d23ae8
jpg-consulting <opensource@jpg-consulting.es>
2016-11-28 16:18:14 +0100

    added GetAppAttributesAsync method

--- IPhoneNotifications/AppleNotificationCenterService/ControlPoint.cs
+++ IPhoneNotifications/AppleNotificationCenterService/ControlPoint.cs
@@ -39,6 +39,67 @@ namespace IPhoneNotifications.AppleNotificationCenterService
         }
 
         /// <summary>
+        /// The Get App Attributes command allows an NC to retrieve attributes of a specific app installed on the NP.
+        /// </summary>
+        /// <param name="appIdentifier">The string identifier of the app the client wants information about.</param>
+        /// <param name="attributeIDs">A list of attributes the NC wants to retrieve.</param>
+        /// <returns></returns>
+        public async Task<GattCommunicationStatus> GetAppAttributesAsync(string appIdentifier, List<AppAttributeID> attributeIDs)
+        {
+            var stream = new MemoryStream();
+            var writer = new BinaryWriter(stream);
+
+            writer.Write((byte)CommandID.GetNotificationAttributes);
+            writer.Write(appIdentifier);
+            writer.Write((byte)0); // App identifier must be a null terminated string
+
+            foreach (NotificationAttributeID attrID in attributeIDs)
+            {
+                writer.Write((byte)attrID);
+            }
+
+            byte[] bytes = stream.ToArray();
+
+            // Send the command
+            try
+            {
+                if (GattCharacteristic.CharacteristicProperties.HasFlag(GattCharacteristicProperties.ReliableWrites))
+                {
+                    GattReliableWriteTransaction transaction = new GattReliableWriteTransaction();
+                    transaction.WriteValue(GattCharacteristic, bytes.AsBuffer());
+                    return await transaction.CommitAsync();
+                }
+                else
+                {
+                    return await GattCharacteristic.WriteValueAsync(bytes.AsBuffer(), GattWriteOption.WriteWithResponse);
+                }
+            }
+            catch (Exception e)
+            {
+                switch ((uint)e.HResult)
+                {
+                    case 0xE04200A0:
+                        System.Diagnostics.Debug.WriteLine("Unknown command. The commandID was not recognized by the NP.");
+                        return GattCommunicationStatus.Success;
+                    case 0xE04200A1:
+                        System.Diagnostics.Debug.WriteLine("Invalid command. The command was improperly formatted.");
+                        return GattCommunicationStatus.Success;
+                    case 0xE04200A2:
+                        System.Diagnostics.Debug.WriteLine("Invalid parameter. One of the parameters (for example, the NotificationUID) does not refer to an existing object on the NP.");
+                        return GattCommunicationStatus.Success;
+                    case 0xE04200A3:
+                        System.Diagnostics.Debug.WriteLine("Action failed. The action was not performed.");
+                        return GattCommunicationStatus.Success;
+                    default:
+                        System.Diagnostics.Debug.WriteLine("Failed to get notification attributes. " + e.Message);
+                        break;
+                }
+
+                throw e;
+            }
+        }
+
+        /// <summary>
         /// The Get Notification Attributes command allows an NC to retrieve the attributes of a specific iOS notification.
         /// </summary>
         /// <param name="notificationUID">The 32-bit numerical value representing the UID of the iOS notification for which the client wants information.</param>
