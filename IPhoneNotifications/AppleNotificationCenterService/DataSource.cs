e8524b1f7c84b8b55fbd350da64ee3706a8b7e0a
jpg-consulting <opensource@jpg-consulting.es>
2016-11-29 18:07:13 +0100

    Dumb minor fix (Should be done better) Small GUI update

--- IPhoneNotifications/AppleNotificationCenterService/DataSource.cs
+++ IPhoneNotifications/AppleNotificationCenterService/DataSource.cs
@@ -13,6 +13,7 @@ namespace IPhoneNotifications.AppleNotificationCenterService
     public class DataSource
     {
         public readonly GattCharacteristic GattCharacteristic;
+        private bool isValueChangedHandlerRegistered = false;
 
         public event Action<NotificationAttributeCollection> NotificationAttributesReceived;
         public event Action<ApplicationAttributeCollection> ApplicationAttributesReceived;
@@ -20,7 +21,6 @@ namespace IPhoneNotifications.AppleNotificationCenterService
         public DataSource(GattCharacteristic characteristic)
         {
             GattCharacteristic = characteristic;
-            GattCharacteristic.ValueChanged += GattCharacteristic_ValueChanged;
         }
 
         public GattCharacteristicProperties CharacteristicProperties
@@ -31,11 +31,58 @@ namespace IPhoneNotifications.AppleNotificationCenterService
             }
         }
 
+        private void AddValueChangedHandler()
+        {
+            if (!isValueChangedHandlerRegistered)
+            {
+                GattCharacteristic.ValueChanged += GattCharacteristic_ValueChanged;
+                isValueChangedHandlerRegistered = true;
+            }
+        }
+
+        private void RemoveValueChangedHandler()
+        {
+            if (isValueChangedHandlerRegistered)
+            {
+                GattCharacteristic.ValueChanged -= GattCharacteristic_ValueChanged;
+                isValueChangedHandlerRegistered = false;
+            }
+        }
+
+        public async void UnsubscribeAsync()
+        {
+            // GattReadClientCharacteristicConfigurationDescriptorResult result = await GattCharacteristic.ReadClientCharacteristicConfigurationDescriptorAsync();
+            RemoveValueChangedHandler();
+        }
+
         public async void SubscribeAsync()
         {
             if (GattCharacteristic.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Notify))
             {
-                await GattCharacteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                try
+                {
+                    // BT_Code: Must write the CCCD in order for server to send notifications.
+                    // We receive them in the ValueChanged event handler.
+                    // Note that this sample configures either Indicate or Notify, but not both.
+                    var result = await
+                            GattCharacteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                                GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                    if (result == GattCommunicationStatus.Success)
+                    {
+                        AddValueChangedHandler();
+                        //rootPage.NotifyUser("Successfully registered for notifications", NotifyType.StatusMessage);
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Error registering for notifications: {result}");
+                        // TODO: Error
+                        //rootPage.NotifyUser($"Error registering for notifications: {result}", NotifyType.ErrorMessage);
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e.Message);
+                }
             }
             else
             {
--- IPhoneNotifications/AppleNotificationCenterService/NotificationSource.cs
+++ IPhoneNotifications/AppleNotificationCenterService/NotificationSource.cs
@@ -25,6 +25,7 @@ namespace IPhoneNotifications.AppleNotificationCenterService
     public class NotificationSource
     {
         public readonly GattCharacteristic GattCharacteristic;
+        private bool isValueChangedHandlerRegistered = false;
 
         public event Action<NotificationSourceData> ValueChanged;
 
@@ -39,7 +40,24 @@ namespace IPhoneNotifications.AppleNotificationCenterService
         public NotificationSource(GattCharacteristic characteristic)
         {
             GattCharacteristic = characteristic;
-            GattCharacteristic.ValueChanged += GattCharacteristic_ValueChanged;
+        }
+
+        private void AddValueChangedHandler()
+        {
+            if (!isValueChangedHandlerRegistered)
+            {
+                GattCharacteristic.ValueChanged += GattCharacteristic_ValueChanged;
+                isValueChangedHandlerRegistered = true;
+            }
+        }
+
+        private void RemoveValueChangedHandler()
+        {
+            if (isValueChangedHandlerRegistered)
+            {
+                GattCharacteristic.ValueChanged -= GattCharacteristic_ValueChanged;
+                isValueChangedHandlerRegistered = false;
+            }
         }
 
         private void GattCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
@@ -56,11 +74,40 @@ namespace IPhoneNotifications.AppleNotificationCenterService
             ValueChanged?.Invoke(dat);
         }
 
+        public async void UnsubscribeAsync()
+        {
+            // GattReadClientCharacteristicConfigurationDescriptorResult result = await GattCharacteristic.ReadClientCharacteristicConfigurationDescriptorAsync();
+            RemoveValueChangedHandler();
+        }
+
         public async void SubscribeAsync()
         {
             if (GattCharacteristic.CharacteristicProperties.HasFlag(GattCharacteristicProperties.Notify))
             {
-                await GattCharacteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                try
+                {
+                    // BT_Code: Must write the CCCD in order for server to send notifications.
+                    // We receive them in the ValueChanged event handler.
+                    // Note that this sample configures either Indicate or Notify, but not both.
+                    var result = await
+                            GattCharacteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                                GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                    if (result == GattCommunicationStatus.Success)
+                    {
+                        AddValueChangedHandler();
+                        //rootPage.NotifyUser("Successfully registered for notifications", NotifyType.StatusMessage);
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Error registering for notifications: {result}");
+                        // TODO: Error
+                        //rootPage.NotifyUser($"Error registering for notifications: {result}", NotifyType.ErrorMessage);
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e.Message);
+                }
             }
             else
             {
